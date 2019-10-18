65818b3c12971bf6a05d47e28b944a1ac8e3bb8a
miguelfranken <miguel.franken@rwth-aachen.de>
2019-10-18 18:41:41 +0200

    fixed e2e tests

--- 02.Frontend/e2e/src/app.e2e-spec.ts
+++ 02.Frontend/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to osc-frontend!');
+    expect(page.getTitleText()).toEqual('OSC Dashboard');
   });
 
   afterEach(async () => {
