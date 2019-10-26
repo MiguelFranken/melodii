4953914c2008c8029b63a46d4d1cbd5290685294
miguelfranken <miguel.franken@rwth-aachen.de>
2019-10-26 13:12:46 +0200

    added registry

--- 01.Backend/src/decorators/container.ts
+++ 01.Backend/src/decorators/container.ts
@@ -1,48 +1,59 @@
-/**
- * Container to be used for inversion of control (IoC).
- * See https://www.tutorialsteacher.com/ioc for some helpful information about IoC
- */
-import { Foo } from "../controllers/foo";
 import { SocketServer } from "../socket/socket-server";
 
+class Registry {
+
+  protected _registryMap = new Map<Function, any>();
+
+  public get(key: Function): any | null {
+    return this._registryMap.get(key);
+  }
+
+  public set(key: Function, value: any): any {
+    this._registryMap.set(key, value);
+    return value;
+  }
+
+  public has(key: Function): boolean {
+    return this._registryMap.has(key);
+  }
+
+}
+
+/**
+ * Container for inversion of control (IoC).
+ * See https://www.tutorialsteacher.com/ioc for some helpful information about IoC
+ */
 export class Container {
 
   // registry of all controller instances
-  private controllerInstances: Array<{ type: Function, object: any }> = [];
+  private controllerRegistry: Registry = new Registry();
 
   // registry of all controller´s dependencies
-  private controllerDependencies: Array<{ type: Function, object: any }> = [];
+  private dependenciesRegistry: Registry = new Registry();
 
   constructor(private socketServer: SocketServer) {
     // add socket server instance to registry
-    const instance = { type: SocketServer, object: socketServer };
-    this.controllerDependencies.push(instance);
+    this.dependenciesRegistry.set(SocketServer, socketServer);
   }
 
   private resolve<T>(someControllerClass: new (...args: any[]) => T): T {
     // find or create new instances for dependencies of the controller
     const ctorParams: any[] = Reflect.getMetadata("design:paramtypes", someControllerClass) || [];
     let params = ctorParams.map((param) => {
-      let dependencyInstance = this.controllerDependencies.find((instance: any) => instance.type === param);
-      if (!dependencyInstance) {
-        dependencyInstance = { type: param, object: new param() };
-        this.controllerDependencies.push(dependencyInstance);
+      if (this.dependenciesRegistry.has(param)) {
+        return this.dependenciesRegistry.get(param);
+      } else {
+        return this.dependenciesRegistry.set(param, new param());
       }
-
-      return dependencyInstance.object;
     });
 
-    // find controller class in registry
-    let instance = this.controllerInstances.find((instance: any) => instance.type === someControllerClass);
-
-    // create new instance if nothing was found
-    if (!instance) {
-      instance = { type: someControllerClass, object: new someControllerClass(...params) };
-      this.controllerInstances.push(instance);
+    // find controller class in controller registry
+    if (this.controllerRegistry.has(someControllerClass)) {
+      return this.controllerRegistry.get(someControllerClass);
+    } else {
+      // create new instance if nothing was found and then return this instance
+      return this.controllerRegistry.set(someControllerClass, new someControllerClass(...params));
     }
-
-    // returns the actual instance
-    return instance.object;
   }
 
   /**
