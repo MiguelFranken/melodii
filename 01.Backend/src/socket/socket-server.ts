d30ec1954827e1fa24195aeabd361fa9c29ea82b
miguelfranken <miguel.franken@rwth-aachen.de>
2019-10-27 12:14:32 +0100

    Added some comments to socket server class

--- 01.Backend/src/socket/socket-server.ts
+++ 01.Backend/src/socket/socket-server.ts
@@ -4,6 +4,9 @@ import * as socketIo from 'socket.io';
 import { Logger } from '@overnightjs/logger';
 import { Event } from './socket-events';
 
+/**
+ * Socket server for bi-directional communication with the Angular frontend.
+ */
 export class SocketServer {
   public static readonly PORT: number = 8080;
   private readonly app: express.Application;
@@ -19,11 +22,29 @@ export class SocketServer {
     this.listen();
   }
 
-  public emit(event: Event, data: any) {
-    Logger.Info(`Emitting event ${event}`);
-    this.io.emit(event, data);
+  /**
+   * Emits some event to the frontend.
+   * @param event Predefined event that the Frontend & Backend both know
+   * @param data Optional data that is being sent with the event
+   */
+  public emit(event: Event, data?: any) {
+    Logger.Info(`Emitting event ${event} to Angular`);
+    if (data) {
+      this.io.emit(event, data);
+    } else {
+      this.io.emit(event);
+    }
   }
 
+  /**
+   * Listen for events sent by the frontend.
+   *
+   * TODO MF: - Maybe we should distinguish between output events (events sent to frontend)
+   *            and input events (events received from frontend).
+   *          - If our system will heavily rely on communication from frontend to backend,
+   *            then we should also create decorators for some socket server controllers
+   *            similar to the osc controllers already existing.
+   */
   private listen(): void {
     this.server.listen(this.port, () => {
       Logger.Info(`Running server on port ${this.port}`);
