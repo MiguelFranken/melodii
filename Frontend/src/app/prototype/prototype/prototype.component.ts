97b7d61cfea4a22123547fb482efe35b7e9f80cc
miguelfranken <miguelfranken90@gmail.com>
2019-11-13 18:37:12 +0100

    sending osc messages from frontend to osc server

--- Frontend/src/app/prototype/prototype/prototype.component.ts
+++ Frontend/src/app/prototype/prototype/prototype.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { interval, Subscription } from "rxjs";
+import { SocketService } from "../../shared/socket/socket.service";
+import { Action } from "../../shared/socket/action";
+import { IOSCMessage } from "../../shared/osc/osc-message";
 
 export class RowButton {
   public isPlayed: boolean = false;
   public isActive: boolean = false;
+
+  public oscMessage: IOSCMessage = {
+    address: "/play_note",
+    args: [
+      { type: "s", value: "C4" }
+    ],
+    info: {
+      address: "/play_note",
+      family: "IPv4",
+      port: 80,
+      size: 1,
+    }
+  };
 
   constructor(public id: string) {
   }
 }
 
 const NUMBER_OF_COLUMNS: number = 8;
+const NUMBER_OF_ROWS: number = 4;
 
 @Component({
   selector: 'app-prototype',
@@ -22,52 +39,26 @@ export class PrototypeComponent implements OnInit {
 
   private playSubscription: Subscription;
 
-  public matrix = [
-    [
-      new RowButton("i00"),
-      new RowButton("i01"),
-      new RowButton("id02"),
-      new RowButton("id03"),
-      new RowButton("id04"),
-      new RowButton("id05"),
-      new RowButton("id06"),
-      new RowButton("id07"),
-    ],
-    [
-      new RowButton("i00"),
-      new RowButton("i01"),
-      new RowButton("id02"),
-      new RowButton("id03"),
-      new RowButton("id04"),
-      new RowButton("id05"),
-      new RowButton("id06"),
-      new RowButton("id07"),
-    ],
-    [
-      new RowButton("i00"),
-      new RowButton("i01"),
-      new RowButton("id02"),
-      new RowButton("id03"),
-      new RowButton("id04"),
-      new RowButton("id05"),
-      new RowButton("id06"),
-      new RowButton("id07"),
-    ],
-    [
-      new RowButton("i00"),
-      new RowButton("i01"),
-      new RowButton("id02"),
-      new RowButton("id03"),
-      new RowButton("id04"),
-      new RowButton("id05"),
-      new RowButton("id06"),
-      new RowButton("id07"),
-    ],
-  ];
+  public matrix = [];
 
-  constructor() { }
+  constructor(private socketService: SocketService) { }
 
   ngOnInit() {
+    this.socketService.initSocket();
+    this.createMatrix();
+  }
+
+  private createMatrix() {
+    for (let i = 0; i < NUMBER_OF_ROWS; i++) {
+      let row: RowButton[] = [];
+
+      for (let y = 0; y < NUMBER_OF_COLUMNS; y++) {
+        const button = new RowButton("id" + i + y);
+        row.push(button);
+      }
+
+      this.matrix.push(row);
+    }
   }
 
   public stop() {
@@ -87,6 +78,10 @@ export class PrototypeComponent implements OnInit {
     for (let rows of this.matrix) {
       const oldButton: RowButton = rows[0];
       oldButton.isPlayed = true;
+
+      if (oldButton.isActive) {
+        this.socketService.send(Action.SEND_OSC_MESSAGE, oldButton.oscMessage);
+      }
     }
 
     this.playSubscription = interval(1000).subscribe(_ => {
@@ -98,6 +93,10 @@ export class PrototypeComponent implements OnInit {
 
         const newButton: RowButton = rows[(this.temp+1) % NUMBER_OF_COLUMNS];
         newButton.isPlayed = true;
+
+        if (newButton.isActive) {
+          this.socketService.send(Action.SEND_OSC_MESSAGE, newButton.oscMessage);
+        }
       }
       this.temp = newTemp;
     })
