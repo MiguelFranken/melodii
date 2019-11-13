3f49a8c21f1b5a7964ad177e7bdcdb80ee1e0c83
miguelfranken <miguelfranken90@gmail.com>
2019-11-13 17:59:17 +0100

    simple matrix

--- Frontend/src/app/prototype/prototype/prototype.component.ts
+++ Frontend/src/app/prototype/prototype/prototype.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Subscription } from "rxjs";
+
+export class RowButton {
+  public isPlayed: boolean = false;
+  public isActive: boolean = false;
+
+  constructor(public id: string) {
+  }
+}
+
+const NUMBER_OF_COLUMNS: number = 8;
 
 @Component({
   selector: 'app-prototype',
@@ -7,9 +18,93 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrototypeComponent implements OnInit {
 
+  public temp: number = 0;
+
+  private playSubscription: Subscription;
+
+  public matrix = [
+    [
+      new RowButton("i00"),
+      new RowButton("i01"),
+      new RowButton("id02"),
+      new RowButton("id03"),
+      new RowButton("id04"),
+      new RowButton("id05"),
+      new RowButton("id06"),
+      new RowButton("id07"),
+    ],
+    [
+      new RowButton("i00"),
+      new RowButton("i01"),
+      new RowButton("id02"),
+      new RowButton("id03"),
+      new RowButton("id04"),
+      new RowButton("id05"),
+      new RowButton("id06"),
+      new RowButton("id07"),
+    ],
+    [
+      new RowButton("i00"),
+      new RowButton("i01"),
+      new RowButton("id02"),
+      new RowButton("id03"),
+      new RowButton("id04"),
+      new RowButton("id05"),
+      new RowButton("id06"),
+      new RowButton("id07"),
+    ],
+    [
+      new RowButton("i00"),
+      new RowButton("i01"),
+      new RowButton("id02"),
+      new RowButton("id03"),
+      new RowButton("id04"),
+      new RowButton("id05"),
+      new RowButton("id06"),
+      new RowButton("id07"),
+    ],
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public stop() {
+    if (this.playSubscription) {
+      this.playSubscription.unsubscribe();
+
+      for (let rows of this.matrix) {
+        const oldButton: RowButton = rows[this.temp];
+        oldButton.isPlayed = false;
+      }
+
+      this.temp = 0;
+    }
+  }
+
+  public start() {
+    for (let rows of this.matrix) {
+      const oldButton: RowButton = rows[0];
+      oldButton.isPlayed = true;
+    }
+
+    this.playSubscription = interval(1000).subscribe(_ => {
+      const newTemp = (this.temp + 1) % NUMBER_OF_COLUMNS;
+
+      for (let rows of this.matrix) {
+        const oldButton: RowButton = rows[this.temp];
+        oldButton.isPlayed = false;
+
+        const newButton: RowButton = rows[(this.temp+1) % NUMBER_OF_COLUMNS];
+        newButton.isPlayed = true;
+      }
+      this.temp = newTemp;
+    })
+  }
+
+  public switch(row: number, column: number) {
+    this.matrix[row][column].isActive = !this.matrix[row][column].isActive;
   }
 
 }
