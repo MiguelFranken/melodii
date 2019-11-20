7a3f3360466af7b1fba84c3c14c65af590f71315
Johannes Maas <johannes.maas1@rwth-aachen.de>
2019-11-20 15:48:56 +0100

    Add logging to Box

--- Generator/src/music/instruments/box.ts
+++ Generator/src/music/instruments/box.ts
@@ -7,10 +7,12 @@ export class Box {
     private readonly synth = new Tone.PolySynth(64, Tone.Synth).toMaster();
 
     public trigger(note: Note, velocity: Velocity) {
+        console.log(`Trigger with note ${note} and velocity ${velocity}.`)
         this.synth.triggerAttack(note, undefined, velocity);
     }
 
     public release(note: Note) {
+        console.log(`Release with note ${note}.`)
         this.synth.triggerRelease(note);
     }
 }
