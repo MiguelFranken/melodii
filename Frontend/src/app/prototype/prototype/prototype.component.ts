8a2cebf5cfae5ea64dbc28fd47032cef9e457ff7
miguelfranken <miguel.franken@rwth-aachen.de>
2019-11-20 15:14:45 +0100

    #34

--- Frontend/src/app/prototype/prototype/prototype.component.ts
+++ Frontend/src/app/prototype/prototype/prototype.component.ts
@@ -1,37 +1,36 @@
 import { Component, ElementRef, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { interval, Observable, Subject, Subscription } from "rxjs";
-import { SocketService } from "../../shared/socket/socket.service";
-import { Action } from "../../shared/socket/action";
-import { switchMap } from "rxjs/operators";
-import { MatMenuTrigger } from "@angular/material/menu";
-import { NavigationService } from "../../shared/layout/navigation/navigation.service";
-import { Matrix } from "./matrix";
-import { Row } from "./row";
-import { RowButton } from "./row-button";
-import { OutsidePlacement, RelativePosition, Toppy } from "toppy";
-import { ToppyConfig } from "toppy/lib/models";
+import { interval, Observable, Subject, Subscription } from 'rxjs';
+import { SocketService } from '../../shared/socket/socket.service';
+import { Action } from '../../shared/socket/action';
+import { switchMap } from 'rxjs/operators';
+import { MatMenuTrigger } from '@angular/material/menu';
+import { NavigationService } from '../../shared/layout/navigation/navigation.service';
+import { Matrix } from './matrix';
+import { Row } from './row';
+import { RowButton } from './row-button';
+import { OutsidePlacement, RelativePosition, Toppy } from 'toppy';
 
 const NOTES_PENTATONIC_C = [
-  "C",
-  "D",
-  "E",
-  "A",
-  "G"
+  'C',
+  'D',
+  'E',
+  'A',
+  'G'
 ];
 const NOTES_MAJOR_C = [
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "A",
-  "B"
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'A',
+  'B'
 ];
 
 const DEFAULT_BPM = 80;
 
-const NUMBER_OF_COLUMNS: number = 8;
-const NUMBER_OF_ROWS: number = 30;
+const NUMBER_OF_COLUMNS = 8;
+const NUMBER_OF_ROWS = 30;
 
 @Component({
   selector: 'app-prototype',
@@ -40,13 +39,65 @@ const NUMBER_OF_ROWS: number = 30;
 })
 export class PrototypeComponent implements OnInit {
 
-  public height: string = "100%";
+  public set bpm(bpm: number) {
+    this._bpm = bpm;
+    this.msPerBeat = 1000 * 0.5 * (60 / bpm);
+  }
+
+  public get bpm(): number {
+    return this._bpm;
+  }
+
+  constructor(
+    private socketService: SocketService,
+    private navigationService: NavigationService,
+    private _toppy: Toppy) { }
+
+  public height = '100%';
 
   private matrixCollection: Matrix[] = [];
   private matrixCollectionIndex = 0;
   public matrix: Matrix;
 
-  public showVelocity: boolean = false;
+  public showVelocity = false;
+
+  private _bpm: number = DEFAULT_BPM;
+
+  private subject = new Subject();
+  private interval;
+
+  private msPerBeat: number = 1000 * 0.5 * (60 / DEFAULT_BPM); // TODO MF: different default
+
+  public temp = 0;
+
+  private playSubscription: Subscription;
+
+  public showRowNames = true;
+
+  public isClosedNavigation: Observable<boolean>;
+
+  @ViewChild('el', {static: true, read: ElementRef })
+  el: ElementRef;
+
+  @ViewChild('tpl', {static: true})
+  tpl: TemplateRef<any>;
+
+  private overlay;
+
+  // public switchShowRowNames() {
+  //   this.showRowNames = !this.showRowNames;
+  // }
+
+  public isInExpandedMode = true;
+
+  private isInFoldMode = false;
+
+  @ViewChild(MatMenuTrigger, {static: false})
+  contextMenu: MatMenuTrigger;
+
+  contextMenuPosition = { x: '0px', y: '0px' };
+
+  private clicked = false;
 
   public switchVelocity() {
     this.showVelocity = !this.showVelocity;
@@ -59,43 +110,6 @@ export class PrototypeComponent implements OnInit {
   public switchRow(row: Row) {
     row.isExpanded = !row.isExpanded;
   }
-
-  private _bpm: number = DEFAULT_BPM;
-
-  public set bpm(bpm: number) {
-    this._bpm = bpm;
-    this.msPerBeat = 1000 * 0.5 * (60 / bpm);
-  }
-
-  public get bpm(): number {
-    return this._bpm
-  }
-
-  private subject = new Subject();
-  private interval;
-
-  private msPerBeat: number = 1000 * 0.5 * (60 / DEFAULT_BPM); // TODO MF: different default
-
-  public temp: number = 0;
-
-  private playSubscription: Subscription;
-
-  public showRowNames: boolean = true;
-
-  public isClosedNavigation: Observable<boolean>;
-
-  @ViewChild('el', {static: true, read: ElementRef })
-  el: ElementRef;
-
-  @ViewChild('tpl', {static: true})
-  tpl: TemplateRef<any>;
-
-  private overlay;
-
-  constructor(
-    private socketService: SocketService,
-    private navigationService: NavigationService,
-    private _toppy: Toppy) { }
 
   changeBpm(event) {
     this.bpm = event.value;
@@ -129,9 +143,9 @@ export class PrototypeComponent implements OnInit {
     this.isClosedNavigation = this.navigationService.getIsClosedObservable();
     this.isClosedNavigation.subscribe(value => {
       if (value) {
-        this.height = "100%";
+        this.height = '100%';
       } else {
-        this.height = "99.99%"; // safari height fix
+        this.height = '99.99%'; // safari height fix
       }
     });
 
@@ -161,16 +175,10 @@ export class PrototypeComponent implements OnInit {
     if (this.matrixCollectionIndex == 0) {
       this.matrixCollectionIndex = this.matrixCollection.length - 1;
     } else {
-      this.matrixCollectionIndex = (this.matrixCollectionIndex -1) % this.matrixCollection.length;
+      this.matrixCollectionIndex = (this.matrixCollectionIndex - 1) % this.matrixCollection.length;
     }
     this.matrix = this.matrixCollection[this.matrixCollectionIndex];
   }
-
-  // public switchShowRowNames() {
-  //   this.showRowNames = !this.showRowNames;
-  // }
-
-  public isInExpandedMode = true;
 
   public switchAllExpanded() {
     this.isInExpandedMode = !this.isInExpandedMode;
@@ -184,8 +192,6 @@ export class PrototypeComponent implements OnInit {
       return !row.isFolded;
     });
   }
-
-  private isInFoldMode = false;
 
   switchFold() {
     if (this.isInFoldMode) {
@@ -214,30 +220,83 @@ export class PrototypeComponent implements OnInit {
     }
   }
 
-  private createMatrixDrums() {
-    let matrix: Matrix = new Matrix();
-    for (let i = 0; i < 6; i++) {
-      let rowArray: RowButton[] = [];
+  private createKickRow(): Row {
+    const rowArray: RowButton[] = [];
 
-      for (let y = 0; y < NUMBER_OF_COLUMNS; y++) {
-        const randomNote = NOTES_MAJOR_C[Math.floor(Math.random() * NOTES_PENTATONIC_C.length)];
-        const note = randomNote + (i+1);
-        const button = new RowButton(note);
-        rowArray.push(button);
-      }
-
-      const row: Row = new Row(rowArray);
-
-      matrix.rows.push(row);
+    for (let y = 0; y < NUMBER_OF_COLUMNS; y++) {
+      const kickButton = new RowButton();
+      kickButton.setOSCMessage({
+        address: '/drums/kick',
+        args: [],
+        info: {
+          address: '/play_note',
+          family: 'IPv4',
+          port: 80,
+          size: 1,
+        }
+      });
+      rowArray.push(kickButton);
     }
-    matrix.name = "Drums";
+
+    return new Row(rowArray, 'Kick');
+  }
+
+  private createSnareRow(): Row {
+    const rowArray: RowButton[] = [];
+
+    for (let y = 0; y < NUMBER_OF_COLUMNS; y++) {
+      const kickButton = new RowButton();
+      kickButton.setOSCMessage({
+        address: '/drums/snare',
+        args: [],
+        info: {
+          address: '/play_note',
+          family: 'IPv4',
+          port: 80,
+          size: 1,
+        }
+      });
+      rowArray.push(kickButton);
+    }
+
+    return new Row(rowArray, 'Snare');
+  }
+
+  private createHihatRow(): Row {
+    const rowArray: RowButton[] = [];
+
+    for (let y = 0; y < NUMBER_OF_COLUMNS; y++) {
+      // kick
+      const kickButton = new RowButton();
+      kickButton.setOSCMessage({
+        address: '/drums/hihat',
+        args: [],
+        info: {
+          address: '/play_note',
+          family: 'IPv4',
+          port: 80,
+          size: 1,
+        }
+      });
+      rowArray.push(kickButton);
+    }
+
+    return new Row(rowArray, 'Hihat');
+  }
+
+  private createMatrixDrums() {
+    const matrix: Matrix = new Matrix();
+    matrix.rows.push(this.createKickRow());
+    matrix.rows.push(this.createHihatRow());
+    matrix.rows.push(this.createSnareRow());
+    matrix.name = 'Drums';
     this.matrixCollection.push(matrix);
   }
 
   private createMatrixPiano() {
-    let matrix: Matrix = new Matrix();
+    const matrix: Matrix = new Matrix();
     for (let i = 0; i < NUMBER_OF_ROWS; i++) {
-      let rowArray: RowButton[] = [];
+      const rowArray: RowButton[] = [];
 
       const randomNote = NOTES_MAJOR_C[i % 7];
       const note = randomNote + (Math.floor(i / 7));
@@ -252,7 +311,7 @@ export class PrototypeComponent implements OnInit {
 
       matrix.rows.push(row);
     }
-    matrix.name = "Synth";
+    matrix.name = 'Synth';
     this.matrixCollection.push(matrix);
   }
 
@@ -260,7 +319,7 @@ export class PrototypeComponent implements OnInit {
     if (this.playSubscription) {
       this.playSubscription.unsubscribe();
 
-      for (let rows of this.matrix.rows) {
+      for (const rows of this.matrix.rows) {
         const oldButton: RowButton = rows.buttons[this.temp];
         oldButton.isPlayed = false;
       }
@@ -274,7 +333,7 @@ export class PrototypeComponent implements OnInit {
   }
 
   public start() {
-    for (let rows of this.matrix.rows) {
+    for (const rows of this.matrix.rows) {
       const oldButton: RowButton = rows.buttons[0];
       oldButton.isPlayed = true;
 
@@ -286,11 +345,11 @@ export class PrototypeComponent implements OnInit {
     this.playSubscription = this.interval.subscribe(_ => {
       const newTemp = (this.temp + 1) % NUMBER_OF_COLUMNS;
 
-      for (let rows of this.matrix.rows) {
+      for (const rows of this.matrix.rows) {
         const oldButton: RowButton = rows.buttons[this.temp];
         oldButton.isPlayed = false;
 
-        const newButton: RowButton = rows.buttons[(this.temp+1) % NUMBER_OF_COLUMNS];
+        const newButton: RowButton = rows.buttons[(this.temp + 1) % NUMBER_OF_COLUMNS];
         newButton.isPlayed = true;
 
         if (newButton.isActive) {
@@ -311,18 +370,13 @@ export class PrototypeComponent implements OnInit {
     }
   }
 
-  @ViewChild(MatMenuTrigger, {static: false})
-  contextMenu: MatMenuTrigger;
-
-  contextMenuPosition = { x: '0px', y: '0px' };
-
   onLongPress(event, button) {
     event.preventDefault();
 
     if (button.isActive) {
       this.contextMenuPosition.x = event.pageX + 'px';
       this.contextMenuPosition.y = event.pageY + 'px';
-      this.contextMenu.menuData = {'button': button};
+      this.contextMenu.menuData = {button};
       this.contextMenu.openMenu();
     }
   }
@@ -333,7 +387,7 @@ export class PrototypeComponent implements OnInit {
     if (button.isActive) {
       this.contextMenuPosition.x = event.clientX + 'px';
       this.contextMenuPosition.y = event.clientY + 'px';
-      this.contextMenu.menuData = { 'button': button };
+      this.contextMenu.menuData = { button };
       this.contextMenu.openMenu();
     }
   }
@@ -349,8 +403,6 @@ export class PrototypeComponent implements OnInit {
     button.velocity -= 10;
     console.log(button.velocity);
   }
-
-  private clicked: boolean = false;
 
   public clickedSlider() {
     this.clicked = true;
