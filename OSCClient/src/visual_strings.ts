15dc139c794593bf23035bb745b60329526706cb
Philipp Dolfus <dolfus.philipp@gmail.com>
2019-11-20 13:54:05 +0100

    add visual_strings.ts

---
+++ OSCClient/src/visual_strings.ts
@@ -0,0 +1,22 @@
+export default {
+    // navigation
+    EXIT: 'exit',
+    BACK: 'back',
+
+    // root menu
+    SEND: '(a) send (again)',
+    PLAY_NOTE: '(b) play random note',
+    PLAY_SONG: '(c) play song',
+    DRUMS_MENU: '(d) drums menu', // sub menu
+    SETTINGS_MENU: '(e) change settings', // sub menu
+
+    // settings menu
+    CHANGE_ADDRESS: '(a) change address',
+    CHANGE_PORT: '(b) change port',
+    CHANGE_PATH: '(c) change osc path',
+    CHANGE_ARGS: '(d) change osc args',
+
+    // drums menu
+    START_DRUMLOOP: '(a) starts a drum loop',
+    STOP_DRUMLOOP: '(b) stops the running drum Loop',
+};
