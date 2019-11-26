82d677acece908ad62d1311bace55da6c3cbc73d
Y0hy0h <Y0hy0h@users.noreply.github.com>
2019-11-26 11:46:25 +0100

    Improve crackle in arc

--- Generator/src/music/instruments/arc.ts
+++ Generator/src/music/instruments/arc.ts
@@ -1,16 +1,23 @@
-import * as Tone from "tone";
 import { Polyphonizer } from '../polyphonizer';
 import { Note, Velocity, Cents } from '../types';
+import { isVoiceActive } from '../utils';
+import { Synth } from 'tone/build/esm';
 
 export class Arc {
-    private readonly voices = new Polyphonizer(() => new Tone.Synth({ envelope: { attack: 0 } }).toMaster());
+    private readonly voices = new Polyphonizer<Synth>(() => new Synth().toDestination());
 
     public set(note: Note, strength: Velocity) {
-        console.log(`Trigger with note ${note} and velocity ${strength}.`);
+        console.log(`Set with note ${note} and velocity ${strength}.`);
         const voice = this.voices.getVoice(note);
+        const volume = -40 + strength * 40; // In db.
         if (strength > 0) {
-            voice.triggerAttack(note, undefined, strength);
+            voice.volume.linearRampToValueAtTime(volume, "+0.1");
+
+            if (!isVoiceActive(voice)) {
+                voice.triggerAttack(note, undefined, 1);
+            }
         } else {
+            voice.volume.value = 0; // Reset volume for next use.
             voice.triggerRelease();
         }
     }
