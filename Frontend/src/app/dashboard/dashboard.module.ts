d1e8278431bea7b114c98ed91ac2803e29ae7342
miguelfranken <miguelfranken90@gmail.com>
2019-11-29 23:53:54 +0100

    removed old unused echo component

--- Frontend/src/app/dashboard/dashboard.module.ts
+++ Frontend/src/app/dashboard/dashboard.module.ts
@@ -3,12 +3,10 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { SocketService } from '../shared/socket/socket.service';
 import { SharedModule } from '../shared/shared.module';
-import { EchoComponent } from './echo/echo.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    EchoComponent,
   ],
   exports: [
     DashboardComponent
