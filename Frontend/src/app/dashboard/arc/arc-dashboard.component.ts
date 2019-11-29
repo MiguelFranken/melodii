4d9d813b3fe32b506a0b819decdfb4c268986c67
miguelfranken <miguelfranken90@gmail.com>
2019-11-30 00:00:58 +0100

    created components for dashboard cards

---
+++ Frontend/src/app/dashboard/arc/arc-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mcp-arc-dashboard',
+  templateUrl: './arc-dashboard.component.html',
+  styleUrls: ['./arc-dashboard.component.scss']
+})
+export class ArcDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
