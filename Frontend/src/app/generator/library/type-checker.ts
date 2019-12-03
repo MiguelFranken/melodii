099a053a4e7a427d566327935ff732ab2e11fa2c
Philipp Dolfus <dolfus.philipp@gmail.com>
2019-12-03 14:01:15 +0100

    changed regex for velocity and changed typecheckerfunctions parameters to accept only an argument and not checking which position

--- Frontend/src/app/generator/library/type-checker.ts
+++ Frontend/src/app/generator/library/type-checker.ts
@@ -4,50 +4,37 @@ import { Cents, Duration, Note, Velocity } from './types';
 
 export class TypeChecker {
 
+  private static regex = {
+    note: /^[A-G][b#]?[0-9]$/,
+    velocity: /^1$|^0$|^0.[0-9]+$/,
+    duration: /^$/,
+    cents: /^$/,
+  };
+
   constructor() {
   }
 
-  public static ValidNote(args: IOSCArg[]): Note {
-    const { type } = args[0];
-    const value = args[0].value.toString();
-    const regex = /^[A-G][b#][0-9]$/;
+  public static ValidNoteArg(arg: IOSCArg) {
+    const { type, value } = arg;
+    const parsed = String(value);
     if (type !== "s") {
       throw new OSCError("MCPx0000", "Note has invalid type");
-    } else if (!value.match(regex)) {
+    } else if (!parsed.match(TypeChecker.regex.velocity)) {
       throw new OSCError("MCPx0001", "Note has invalid value");
-    }
-    return value;
-  }
-
-  public static ValidVelocityArg(arg: IOSCArg) {
-    const regex = /^[0-9.]$/; // TODO MF: Das ist falsch. Es gehen auch Werte durch die nicht in der Range sind.
-    const { type, value } = arg;
-    const parsed = parseFloat(value.toString());
-    if (type !== "f") {
-      throw new OSCError("MCPx0002", "Velocity has invalid type", arg);
-    } else if (!String(value).match(regex)) {
-      // velocity should be in normal range ([0,1])
-      throw new OSCError("MCPx0003", "Velocity value is not in normal range ([0,1])", arg);
-    } else if (isNaN(parsed)) {
-      throw new OSCError("MCPx0004", "Velocity value matches not the right type", arg);
     }
     return parsed;
   }
 
-  public static ValidVelocity(args: IOSCArg[]): Velocity | undefined {
-    if (args.length < 2) {
-      return undefined;
-    }
-    const regex = /^[0-9.]+$/; // TODO MF: Das ist wohl falsch. Es gehen auch Werte durch die nicht in der Range sind.
-    const { type, value } = args[1];
+  public static ValidVelocityArg(arg: IOSCArg) {
+    const { type, value } = arg;
     const parsed = parseFloat(value.toString());
     if (type !== "f") {
-      throw new OSCError("MCPx0002", "Velocity has invalid type");
-    } else if (!String(value).match(regex)) {
+      throw new OSCError("MCPx0002", "Velocity has invalid type", arg);
+    } else if (!String(parsed).match(TypeChecker.regex.velocity)) {
       // velocity should be in normal range ([0,1])
-      throw new OSCError("MCPx0003", "Velocity value is not in normal range ([0,1])");
+      throw new OSCError("MCPx0003", "Velocity value is not in normal range ([0,1])", arg);
     } else if (isNaN(parsed)) {
-      throw new OSCError("MCPx0004", "Velocity value matches not the right type");
+      throw new OSCError("MCPx0004", "Velocity value matches not the right type", arg);
     }
     return parsed;
   }
@@ -61,22 +48,9 @@ export class TypeChecker {
     return value;
   }
 
-  public static ValidDuration(args: IOSCArg[]): Duration | undefined {
-    if (args.length < 3) {
-      return undefined;
-    }
-    const { type, value } = args[2];
-    if (type !== "s") {
-      throw new OSCError("MCPx0005", "Duration has invalid type");
-    }
-    // TODO
-    return value;
-  }
-
-  public static ValidCents(args: IOSCArg[]): Cents | undefined {
-    // TODO
-    const { type, value } = args[1];
+  public static ValidCentsArg(arg: IOSCArg): Cents | undefined {
+    
     // return value;
-    return value as number;
+    return arg.value as number;
   }
 }
