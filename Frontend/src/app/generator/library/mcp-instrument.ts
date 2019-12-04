970c9644e1cc27c1b79c97a576f3f6adffe13f1e
Johannes Maas <johannes.maas1@rwth-aachen.de>
2019-12-04 15:25:26 +0100

    Fix IMCPInstraument

--- Frontend/src/app/generator/library/mcp-instrument.ts
+++ Frontend/src/app/generator/library/mcp-instrument.ts
@@ -1,10 +1,8 @@
-import { Instrument } from 'tone/build/esm/instrument/Instrument';
-import { Polyphonizer } from './polyphonizer';
 import { ToneAudioNode } from 'tone';
 
 export type MCPInstrumentName = string;
 
 export interface IMCPInstrument {
   name: MCPInstrumentName;
-  getAudioNode(): ToneAudioNode | Polyphonizer<any>; // TODO MF: Lösung für den Polyphonizer notwendig!
+  getAudioNode(): ToneAudioNode;
 }
