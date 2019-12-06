ddb7984007c23e80625bcfcf9646f948bbd6c335
miguelfranken <miguelfranken90@gmail.com>
2019-12-06 19:40:09 +0100

    added drag and drop example

--- Frontend/src/app/box/box.component.ts
+++ Frontend/src/app/box/box.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+import { Draggable, Droppable, Swappable } from '@shopify/draggable';
 
 @Component({
   selector: 'mcp-box',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoxComponent implements OnInit {
 
+  @ViewChild('block', { static: true })
+  block: ElementRef<HTMLElement>;
+
   constructor() { }
 
   ngOnInit() {
+    const swappable = new Swappable(this.block.nativeElement, {
+      draggable: '.item',
+    });
+
+    swappable.on('swappable:start', (event) => {
+      console.log('swappable:start', event.data.dragEvent.source);
+    });
+
+    swappable.on('swappable:swapped', (event) => console.log('swappable:swapped', event));
+    swappable.on('swappable:stop', (event) => console.log('swappable:stop', event));
   }
 
 }
