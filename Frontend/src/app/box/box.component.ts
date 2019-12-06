50bedaac49da7b03f1d8628564d563b6bf022533
miguelfranken <miguelfranken90@gmail.com>
2019-12-06 22:57:59 +0100

    switched from box component to mat component

--- Frontend/src/app/box/box.component.ts
+++ Frontend/src/app/box/box.component.ts
@@ -1,61 +1,9 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-
-import { Swappable } from '@shopify/draggable';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'mcp-box',
   templateUrl: './box.component.html',
   styleUrls: ['./box.component.scss']
 })
-export class BoxComponent implements OnInit {
-
-  public data = ["C", "D", "E", "F", "G"];
-  public orderedIDs = [0, 1, 2, 3, 4];
-
-  @ViewChild('block', { static: true })
-  block: ElementRef<HTMLElement>;
-
-  private sourceId: number | null;
-  private targetId: number | null;
-
-  private static Swap(array, sourceID, targetID) {
-    const b = array[sourceID];
-    array[sourceID] = array[targetID];
-    array[targetID] = b;
-  }
-
-  constructor() { }
-
-  private getDataIndexFromId(id: number) {
-    return this.orderedIDs.findIndex((i) => i === +id);
-  }
-
-  getNote(id: number): string {
-    const dataIndex = this.getDataIndexFromId(id);
-    return this.data[dataIndex];
-  }
-
-  ngOnInit() {
-    const swappable = new Swappable(this.block.nativeElement, {
-      draggable: '.item',
-      mirror: {
-        appendTo: 'body'
-      }
-    });
-
-    swappable.on('swappable:swapped', (event) => {
-      this.sourceId = +event.data.dragEvent.data.source.firstElementChild.id;
-      this.targetId = +event.data.dragEvent.data.over.firstElementChild.id;
-    });
-
-    swappable.on('drag:stop', (event) => {
-      if (this.sourceId != null && this.targetId != null) {
-        BoxComponent.Swap(this.data, this.getDataIndexFromId(this.sourceId), this.getDataIndexFromId(this.targetId));
-        BoxComponent.Swap(this.orderedIDs, this.getDataIndexFromId(this.sourceId), this.getDataIndexFromId(this.targetId));
-        this.targetId = null;
-        this.sourceId = null;
-      }
-    });
-  }
-
+export class BoxComponent {
 }
--- Frontend/src/app/mat/mat.component.ts
+++ Frontend/src/app/mat/mat.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Swappable } from '@shopify/draggable';
 
 @Component({
   selector: 'mcp-mat',
@@ -7,9 +8,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MatComponent implements OnInit {
 
+  public data = ["C", "D", "E", "F", "G"];
+  public orderedIDs = [0, 1, 2, 3, 4];
+
+  @ViewChild('block', { static: true })
+  block: ElementRef<HTMLElement>;
+
+  private sourceId: number | null;
+  private targetId: number | null;
+
+  private static Swap(array, sourceID, targetID) {
+    const b = array[sourceID];
+    array[sourceID] = array[targetID];
+    array[targetID] = b;
+  }
+
   constructor() { }
 
+  private getDataIndexFromId(id: number) {
+    return this.orderedIDs.findIndex((i) => i === +id);
+  }
+
+  getNote(id: number): string {
+    const dataIndex = this.getDataIndexFromId(id);
+    return this.data[dataIndex];
+  }
+
   ngOnInit() {
+    const swappable = new Swappable(this.block.nativeElement, {
+      draggable: '.item',
+      mirror: {
+        appendTo: 'body'
+      }
+    });
+
+    swappable.on('swappable:swapped', (event) => {
+      this.sourceId = +event.data.dragEvent.data.source.firstElementChild.id;
+      this.targetId = +event.data.dragEvent.data.over.firstElementChild.id;
+    });
+
+    swappable.on('drag:stop', (event) => {
+      if (this.sourceId != null && this.targetId != null) {
+        MatComponent.Swap(this.data, this.getDataIndexFromId(this.sourceId), this.getDataIndexFromId(this.targetId));
+        MatComponent.Swap(this.orderedIDs, this.getDataIndexFromId(this.sourceId), this.getDataIndexFromId(this.targetId));
+        this.targetId = null;
+        this.sourceId = null;
+
+        // TODO MF: Create OSC Message(s) for the changes and send them to the OSC Server
+      }
+    });
   }
 
 }
