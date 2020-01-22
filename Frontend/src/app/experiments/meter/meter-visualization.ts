b2c0465a557cc8777f57929834d4e8b9633cba96
miguelfranken <miguelfranken90@gmail.com>
2020-01-22 01:44:01 +0100

    clamp change in experiments page for testing

--- Frontend/src/app/experiments/meter/meter-visualization.ts
+++ Frontend/src/app/experiments/meter/meter-visualization.ts
@@ -54,8 +54,8 @@ export class MeterVisualization {
 
   private static GetNormalizedMeterValue(meter: Meter): number {
     let meterValue = meter.getValue();
-    meterValue = MeterVisualization.Clamp(meterValue, -80, -10);
-    meterValue = MeterVisualization.Normalize( meterValue, [ -80, -10 ], [ 0, 100 ] );
+    meterValue = MeterVisualization.Clamp(meterValue, -50, -10);
+    meterValue = MeterVisualization.Normalize( meterValue, [ -50, -10 ], [ 0, 100 ] );
     return meterValue;
   }
 
